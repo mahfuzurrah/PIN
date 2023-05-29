b567e2922252938eaa190d0c2bdd89fd88946ed9
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-05-30 01:20:41 +0600

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,10 @@
+// Navbar
+function toggle() {
+    var content = document.getElementById("navbarNav");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
